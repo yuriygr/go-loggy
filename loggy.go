4173702da193233b688bad7475ca02b0d9b87590
Юрий Гринёв <a1d516ac5f5d290@gmail.com>
2020-12-22 21:47:37 +0500

    Цвета теперь не глобальные @TODO: сделать кастомизируемый вывод(?)

--- loggy.go
+++ loggy.go
@@ -6,14 +6,14 @@ import (
 )
 
 var (
-	Black   = color("\033[1;30m%s\033[0m")
-	Red     = color("\033[1;31m%s\033[0m")
-	Green   = color("\033[1;32m%s\033[0m")
-	Yellow  = color("\033[1;33m%s\033[0m")
-	Purple  = color("\033[1;34m%s\033[0m")
-	Magenta = color("\033[1;35m%s\033[0m")
-	Teal    = color("\033[1;36m%s\033[0m")
-	White   = color("\033[1;37m%s\033[0m")
+	black   = color("\033[1;30m%s\033[0m")
+	red     = color("\033[1;31m%s\033[0m")
+	green   = color("\033[1;32m%s\033[0m")
+	yellow  = color("\033[1;33m%s\033[0m")
+	purple  = color("\033[1;34m%s\033[0m")
+	magenta = color("\033[1;35m%s\033[0m")
+	teal    = color("\033[1;36m%s\033[0m")
+	white   = color("\033[1;37m%s\033[0m")
 
 	success = color("\033[1;32m%s\033[0m")
 	info    = color("\033[1;34m%s\033[0m")
